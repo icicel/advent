0969baba7abf34f7d9d9ac18ebee64294a6fda69
icicel <isakham@chalmers.se>
2023-12-02 17:08:49 +0100

    Day template

---
+++ 2023/Template.java
@@ -0,0 +1,27 @@
+import base.Input;
+
+//
+//
+//
+public class Template {
+    public static void main(String[] args) {
+        Template main = new Template();
+        System.out.println(main.solve1());
+        System.out.println(main.solve2());
+    }
+
+
+
+    public Template() {
+    
+    }
+
+    public int solve1() {
+        return 0;
+    }
+
+    public int solve2() {
+        return 0;
+    }
+    
+}
