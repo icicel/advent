61236131e6a5bf13122173115f97925c73100a02
icicel <isakham@chalmers.se>
2023-12-07 22:48:30 +0100

    Improved timer

--- 2023/Timer.java
+++ 2023/Timer.java
@@ -13,28 +13,38 @@ public class Timer {
     }
 
     // featuring some hacky ways of getting vscode to stop complaining
-    public static void time(int dayN) throws Exception {
+    public static void time(int dayNumber) throws Exception {
         try {
-            Class<?> dayX = Class.forName("Day" + dayN);
-            Object day = dayX.getDeclaredConstructor().newInstance();
+            Class<?> day = Class.forName("Day" + dayNumber);
+            List<Long> total = new LinkedList<>();
+            List<Long> init = new LinkedList<>();
             List<Long> part1 = new LinkedList<>();
             List<Long> part2 = new LinkedList<>();
             long finish = System.nanoTime() + seconds * 1000000000;
             do {
+                long totalStart = System.nanoTime();
+
                 long start = System.nanoTime();
-                dayX.getMethod("solve1").invoke(day);
-                part1.add(System.nanoTime() - start);
-                day = dayX.getDeclaredConstructor().newInstance();
+                Object dayObject = day.getDeclaredConstructor().newInstance();
+                init.add(System.nanoTime() - start);
+
                 start = System.nanoTime();
-                dayX.getMethod("solve2").invoke(day);
+                day.getMethod("solve1").invoke(dayObject);
+                part1.add(System.nanoTime() - start);
+
+                start = System.nanoTime();
+                day.getMethod("solve2").invoke(dayObject);
                 part2.add(System.nanoTime() - start);
-                day = dayX.getDeclaredConstructor().newInstance();
+
+                total.add(System.nanoTime() - totalStart);
             } while (System.nanoTime() < finish);
-            System.out.println("Day " + dayN);
+            System.out.println("Day " + dayNumber);
+            System.out.println(" init: " + average(init) + "ms");
             System.out.println(" part 1: " + average(part1) + "ms");
             System.out.println(" part 2: " + average(part2) + "ms");
+            System.out.println(" in total: " + average(total) + "ms");
         } catch (ClassNotFoundException e) {
-            // finish
+            // silently skip
         }
     }
 
